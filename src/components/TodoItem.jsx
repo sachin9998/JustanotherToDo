3d75e27934d8b1d6362e0f0e3749e1f7a04bfb79
Sachin Alam <sachinalam4216@gmail.com>
2024-07-10 22:23:17 +0530

    Final Push

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { useTodo } from "../contexts/TodoContext";
 
-
 function TodoItem({ todo }) {
   const [isTodoEditable, setIsTodoEditable] = useState(false);
   const [todoMsg, setTodoMsg] = useState(todo.todo);
-  const { updateTodo, deleteTodo, toggleComplete } = useTodo;
+  const { updateTodo, deleteTodo, toggleComplete } = useTodo();
 
   const editTodo = () => {
     updateTodo(todo.id, { ...todo, todo: todoMsg });
